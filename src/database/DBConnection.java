9de5624bd651b9a42a4bf9c0705efa8eebea8ebc
YomalHerath <yomalherath1121@gmail.com>
2023-02-12 09:16:00 +0000

    minor changes on db connection

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -11,13 +11,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 public class DBConnection {
 
-    public static Connection getConnection_D() throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/motorbike_management", "root", "");
-        return con;
-    }
-    public static void main(String[] args) {
+    static Connection con;
+    static String driver = "com.mysql.jdbc.Driver";
+    static String url = "jdbc:mysql://localhost/rental_system";
+    static String uname = "root";
+    static String pass = "";
 
+
+    public static Connection getConnection() throws Exception{
+        if(con == null){
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,uname, pass);
+        }
+        return con;
     }
 
 }
