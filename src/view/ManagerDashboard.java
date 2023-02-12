7751610615c70256062a8971a2b3020644daac33
YomalHerath <yomalherath1121@gmail.com>
2023-02-12 01:12:34 +0000

    start to develop manager dashboard gui

---
+++ src/view/ManagerDashboard.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ManagerDashboard extends JDialog {
+    private JPanel ManagerDashboardPanel;
+    private JLabel lblText;
+    private JButton btnAddReservation;
+
+    public ManagerDashboard(JFrame jFrame){
+        super(jFrame);
+        setTitle("Room Rental System");
+        setContentPane(ManagerDashboardPanel);
+        //set minimum size for dialog
+        setMinimumSize(new Dimension(1280,720));
+        setModal(true);
+        //display dialog in the middle of the frame
+        setLocationRelativeTo(jFrame);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        ManagerDashboard dashboard = new ManagerDashboard(null);
+    }
+
+}
