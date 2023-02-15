9900b53850fa3fdffd55927479fbef4264ba51fc
YomalHerath <yomalherath1121@gmail.com>
2023-02-15 11:43:21 +0000

    create gui for add room form

---
+++ src/view/AddRoomDetails.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.*;
+
+public class AddRoomDetails {
+    private JPanel AddRoomDetailsPanel;
+    private JPanel JPanel1;
+    private JPanel JPanel2;
+    private JLabel lblLoginTopic;
+    private JPanel JPanel3;
+    private JLabel lblRoomNo;
+    private JTextField tFieldRoomNo;
+    private JLabel lblRoomType;
+    private JButton btnSignIn;
+    private JComboBox comboBoxRoomType;
+    private JButton btnCancel;
+}
