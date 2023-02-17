7aaea4adf38f2d84fb72482768f96d930e766a53
YomalHerath <yomalherath1121@gmail.com>
2023-02-17 19:12:13 +0000

    create interface for clerks for crud methods

---
+++ src/controller/ClerkInterface.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.Clerk;
+
+import java.util.List;
+
+public interface ClerkInterface {
+    public void save(Clerk clerk);
+    public void update(Clerk clerk);
+    public Clerk get (int clerkId);
+    public List<Clerk> list();
+}
