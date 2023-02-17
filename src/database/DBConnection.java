ea972f7804ee50f67b79d75ba15406deef6b45dc
YomalHerath <yomalherath1121@gmail.com>
2023-02-17 19:11:28 +0000

    make changes on database connection

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,19 +1,10 @@
 package database;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 import java.sql.Connection;
 import java.sql.DriverManager;
 public class DBConnection {
-
     static Connection con;
     static String driver = "com.mysql.jdbc.Driver";
-    static String url = "jdbc:mysql://localhost:3306/rental_system";
+    static String url = "jdbc:mysql://localhost/rental_system";
     static String uname = "root";
     static String pass = "";
 
@@ -21,9 +12,8 @@ public class DBConnection {
     public static Connection getConnection() throws Exception{
         if(con == null){
             Class.forName(driver);
-            con = DriverManager.getConnection(url,uname, pass);
+            con = DriverManager.getConnection(url, uname, pass);
         }
         return con;
     }
-
 }
